f481905ca349a1f38aff2a1aaf0698044438d689
vikkastaneja <vikkastaneja@users.noreply.github.com>
2024-12-30 02:13:15 -0800

    Solved second step of word count problem (-w)
    n this step your goal is to support the command line option -w that outputs the number of words in a file.

--- word_count/ccwc.py
+++ word_count/ccwc.py
@@ -1,23 +1,43 @@
 import sys
 
+def word_count_switch(args):
+    def option_c():
+        filename = sys.argv[2]
+        size = 0
+        with open(filename, "rb") as f:
+            f.seek(0, 2)
+            size = f.tell()
+
+        print(f"{size} {filename}")
+        return size
+
+    def option_w():
+        filename = sys.argv[2]
+        size = 0
+        with open(filename, "r") as f:
+            size = len(f.read().split())
+
+        print(f"{size} {filename}")
+        return size
+
+    def default_case():
+        print("Invalid option")
+        return -1
+
+    switch = {
+        "-c": option_c,
+        "-w": option_w
+    }
+
+    return switch.get(args[1], default_case)()
+
 def word_count(file_path):
     if len(sys.argv) != 3:
-        print("Usage: python ccwc.py -c <filename>")
+        print("Usage: python ccwc.py <options> <filename>")
+        print("Options:\n\t-c\tCount the number of bytes in the file\n\t-w\tCount the number of words in the file")
         sys.exit(1)
 
-    if sys.argv[1] != "-c":
-        print("Usage: python ccwc.py -c <filename>")
-        sys.exit(1)
-
-    filename = sys.argv[2]
-    size = 0
-    with open(filename, "rb") as f:
-        f.seek(0, 2)
-        size = f.tell()
-
-    print(f"{size} {filename}")
-    return size
+    return word_count_switch(file_path)
 
 if __name__ == "__main__":
-
     word_count(sys.argv)
--- word_count/ccwc_test.py
+++ word_count/ccwc_test.py
@@ -2,9 +2,24 @@ import sys
 import os
 
 from ccwc import word_count
+text_file = os.path.dirname(__file__) + "/test.txt"
 
-def test_word_count():
-    sys.argv = ["ccwc.py", "-c", os.path.dirname(__file__) + "/test.txt"]
+def test_word_count_c():
+    sys.argv = ["ccwc.py", "-c", text_file]
     actual = word_count(sys.argv)
 
-    assert actual == 12, "Results are different"
+    assert actual == 12, "Byte counts are different"
+
+
+def test_word_count_w():
+    sys.argv = ["ccwc.py", "-w", text_file]
+    actual = word_count(sys.argv)
+
+    assert actual == 2, "Word counts are different"
+
+def test_word_count_default():
+    sys.argv = ["ccwc.py", "-a", text_file]
+    actual = word_count(sys.argv)
+
+    assert actual == -1, "Default counts are different"
+
